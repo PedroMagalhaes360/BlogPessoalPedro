50e0c0092a9ba62ed39ca921c2f66fdfb0841255
PedroMagalhaes360 <henriquepedro13_@hotmail.com>
2021-06-24 12:08:54 -0300

    Primeiro projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blogPessoalPedro';
+  title = 'Olá sejam bem vindos,';
 }
