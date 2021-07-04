d0c4298be6c0709bde1be484df251dba09ddc763
PedroMagalhaes360 <henriquepedro13_@hotmail.com>
2021-07-04 12:04:48 -0300

    Blog Pessoal 2.0

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { UserLogin } from '../model/UserLogin';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.id = this.userLogin.id
+
+      this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 500){
+        alert('Usuário ou senha estão incorretos!!!')
+      }
+    })
   }
 
 }
